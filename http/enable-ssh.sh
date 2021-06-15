236bc6da2dc3536aac00b563276c1915bbf8173a
Brian Zalewski <brian@megabyte.space>
2021-06-15 02:01:14 +0000

    Update http/enable-ssh.sh

--- http/enable-ssh.sh
+++ http/enable-ssh.sh
@@ -3,7 +3,7 @@
 PASSWORD=$(/usr/bin/openssl passwd -crypt 'vagrant')
 
 # Vagrant-specific configuration
-/usr/bin/useradd --password ${PASSWORD} --comment 'Vagrant User' --create-home --user-group vagrant
+/usr/bin/useradd --password "${PASSWORD}" --comment 'Vagrant User' --create-home --user-group vagrant
 echo 'Defaults env_keep += "SSH_AUTH_SOCK"' >/etc/sudoers.d/vagrant
 echo "vagrant ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/vagrant
 /usr/bin/chmod 0440 /etc/sudoers.d/vagrant
